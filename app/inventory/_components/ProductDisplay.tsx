dc93074a8f2f8bef90025b5bbbeba748058ffe70
harvest7777 <333harvest.moon333@gmail.com>
2025-05-07 06:51:28 -0700

    pictures

--- app/inventory/_components/ProductDisplay.tsx
+++ app/inventory/_components/ProductDisplay.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Product } from "../_data/products";
 import { BsInfoCircle } from "react-icons/bs";
 import ProductInfoModal from "./ProductInfoModal";
@@ -15,13 +16,21 @@ const ProductDisplay: React.FC<ProductDisplayProps> = ({ product }) => {
   };
 
   return (
-    <div className="w-full overflow-hidden rounded-xl outline-1 outline-muted p-2 shadow-sm">
+    <div className="w-full flex flex-col items-center overflow-hidden rounded-xl outline-1 outline-muted p-2 shadow-sm">
       <ProductInfoModal
         product={product}
         visible={visible}
         setVisible={setVisible}
       />
-      <div className="h-40">image would go here</div>
+      <div className="h-40 aspect-square flex justify-center overflow-hidden">
+        <Image
+          src={`/${product.name.replace(/\s+/g, "")}.png`}
+          alt={product.name}
+          width={500}
+          height={500}
+          objectFit="contain"
+        />
+      </div>
       <h1 className="font-bold">{product.name}</h1>
       <h2 className={`${getQuantityColor(product.quantity)}`}>
         Quantity: {product.quantity}
